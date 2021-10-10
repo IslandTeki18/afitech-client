20fe2975f0908ed76a9f501dce73051a2bfd1a41
Landon McKell <landon.roney7923@gmail.com>
2021-10-09 23:16:46 -0600

    start footer

---
+++ src/app/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        
+                    </div>
+                    <div className="col-md-6">
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
