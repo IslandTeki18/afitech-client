92244f065dd47514102b6e2b614f964de3f3687b
Landon McKell <landon.roney7923@gmail.com>
2021-10-21 23:36:21 -0600

    minor changes to display project list card adn page

--- src/components/projectListCard/ProjectListCard.jsx
+++ src/components/projectListCard/ProjectListCard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./ProjectListCard.scss";
 import PropTypes from "prop-types";
 
 const ProjectListCard = (props) => {
     return (
-        <div className="card bg-dark text-white border-0">
+        <div className="dkProjectListCard card bg-dark text-white border-0">
             <img src={props.image} className="card-img" alt={props.altImage} />
-            <div className="card-img-overlay">
+            <div className="card-img-overlay d-flex justify-content-between align-items-center">
                 <h3 className="card-title">{props.title}</h3>
                 <h5 className="card-text">{props.projectType}</h5>
             </div>
