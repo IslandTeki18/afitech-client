5406556444fe1439beb98e16700c620627d15cbc
Landon McKell <landon.roney7923@gmail.com>
2021-10-28 23:50:52 -0600

    start of project details page

---
+++ src/pages/projectDetailsPage/ProjectDetailsPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ProjectDetailsPage.scss";
+
+const ProjectDetailsPage = () => {
+    return (
+        <div className="dkProjectDetailsPage">
+            <section id="pageHeaderSection">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="projectDetailsSection"></section>
+        </div>
+    );
+};
+
+export default ProjectDetailsPage;
