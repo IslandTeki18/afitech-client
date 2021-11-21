33675e9c9bff1616d2de2271d7b4c12788e4f412
Landon McKell <landon.roney7923@gmail.com>
2021-11-20 23:21:35 -0700

    Refactor: Redesigned the footer. Chore: Linking sub items to only tag pages

--- src/app/footer/Footer.jsx
+++ src/app/footer/Footer.jsx
@@ -1,69 +1,114 @@
 import React from "react";
-import ListGroup from "../../components/listGroup/ListGroup";
-import ListGroupItem from "../../components/listGroupItem/ListGroupItem";
-import Icon from "../../components/icon/Icon";
+import "./Footer.scss";
 import { Link } from "react-router-dom";
+import Icon from "../../components/icon/Icon";
 
 const Footer = () => {
-    const menuItems = ["Home", "Projects", "Services", "About", "Contact"];
+    const serviceNavItems = {
+        header: "service",
+        items: [
+            { name: "All Services", link: "/all-services" },
+            { name: "One Page Websites", link: "/services/one-page" },
+            { name: "Full Stack Websites", link: "/services/full-stack" },
+            { name: "Mobile Apps", link: "/services/mobile-app" },
+        ],
+    };
+    const projectNavItems = {
+        header: "project",
+        items: [
+            { name: "All Projects", link: "/projects" },
+            { name: "One Page Websites", link: "/projects" },
+            { name: "Full Stack Websites", link: "/projects" },
+            { name: "Mobile Apps", link: "/projects" },
+        ],
+    };
+    const blogNavItems = {
+        header: "blog",
+        items: [
+            { name: "All Posts", link: "/blogs" },
+            { name: "ReactJS", link: "/blogs" },
+            { name: "NodeJS", link: "/blogs" },
+            { name: "Redux", link: "/blogs" },
+            { name: "React Native", link: "/blogs" },
+        ],
+    };
     const socialItems = [
-        { name: "Instagram", link: "www.google.com" },
-        { name: "Facebook", link: "www.google.com" },
-        { name: "Twitter", link: "www.google.com" },
-        { name: "Github", link: "www.google.com" },
-        { name: "Google+", link: "www.google.com" },
+        { name: "instagram", link: "www.google.com" },
+        { name: "facebook", link: "www.google.com" },
+        { name: "twitter", link: "www.google.com" },
+        { name: "github", link: "www.google.com" },
+        { name: "google", link: "www.google.com" },
     ];
+
+    function renderSocialItems() {
+        return socialItems.map((social, idx) => (
+            <li className="list-inline-item list-social-item me-3" key={idx}>
+                <a
+                    href={social.link}
+                    rel="noreferrer noopener"
+                    target="_blank"
+                    className="text-decoration-none"
+                >
+                    <Icon
+                        icon={social.name}
+                        iconStyle="fab"
+                        size="14"
+                        color="#ffffff"
+                    />
+                </a>
+            </li>
+        ));
+    }
+
+    function renderNavLists(navList) {
+        return navList.items.map((item, idx) => (
+            <li className="mb-3">
+                <Link
+                    to={item.link}
+                    className="text-reset text-decoration-none"
+                >
+                    {item.name}
+                </Link>
+            </li>
+        ));
+    }
 
     // TODO: Make Mobile Responsive
     // TODO: Add links to footer
     return (
-        <footer className="mt-auto bg-dark py-5">
+        <footer className="dkFooter mt-auto bg-dark py-5">
             <div className="container">
-                <div className="row">
-                    <div className="col-md-3 text-white text-center border-end">
-                        <ListGroup>
-                            <ListGroupItem isBold>Menu</ListGroupItem>
-                            {menuItems.map((item, idx) => (
-                                <Link to="/" key={idx}>
-                                    <ListGroupItem>{item}</ListGroupItem>
-                                </Link>
-                            ))}
-                        </ListGroup>
+                <div className="row text-white">
+                    <div className="col-12 col-md-3">
+                        <h1>LOGO</h1>
+                        <p className="text-muted">Ideas that come to life.</p>
+                        <ul className="list-unstyled list-inline list-social mb-6 mb-md-0">
+                            {renderSocialItems()}
+                        </ul>
                     </div>
-                    <div className="col-md-6 text-white text-center">
-                        <h3>AFI TECH</h3>
-                        <p>
-                            We specialize in developing fast and reliant web and
-                            mobile applications for your business or for your
-                            personal requests. We strive to be fast in all
-                            aspects that go into development such as planning,
-                            designing, developing, deploying, and maintaining.
-                        </p>
-                        <h6 className="pt-3">
-                            Made with{" "}
-                            <Icon
-                                iconStyle="fas"
-                                icon="heart"
-                                size="15"
-                                color="red"
-                            />{" "}
-                            by Landon McKell
+                    <div className="col-6 col-md-3">
+                        <h6 className="fw-bold text-uppercase text-gray-700">
+                            {serviceNavItems.header}
                         </h6>
+                        <ul className="list-unstyled text-muted mb-6 mb-md-8 md-lg-0">
+                            {renderNavLists(serviceNavItems)}
+                        </ul>
                     </div>
-                    <div className="col-md-3 text-white text-center border-start">
-                        <ListGroup>
-                            <ListGroupItem isBold>Social Media</ListGroupItem>
-                            {socialItems.map((item) => (
-                                <a
-                                    href={item.link}
-                                    rel="noreferrer noopener"
-                                    target="_blank"
-                                    key={item.name}
-                                >
-                                    <ListGroupItem>{item.name}</ListGroupItem>
-                                </a>
-                            ))}
-                        </ListGroup>
+                    <div className="col-6 col-md-3">
+                        <h6 className="fw-bold text-uppercase text-gray-700">
+                            {blogNavItems.header}
+                        </h6>
+                        <ul className="list-unstyled text-muted mb-6 mb-md-8 md-lg-0">
+                            {renderNavLists(blogNavItems)}
+                        </ul>
+                    </div>
+                    <div className="col-6 col-md-3">
+                        <h6 className="fw-bold text-uppercase text-gray-700">
+                            {projectNavItems.header}
+                        </h6>
+                        <ul className="list-unstyled text-muted mb-6 mb-md-8 md-lg-0">
+                            {renderNavLists(projectNavItems)}
+                        </ul>
                     </div>
                 </div>
             </div>
