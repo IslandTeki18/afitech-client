10a6f43ba9323b14e7ee2ca275fa3a3fa49d3e6b
Landon McKell <landon.roney7923@gmail.com>
2021-11-21 21:26:02 -0700

    Feature: Added a vs code extension that compiles my custom scss file for bootstrap for me to use my own theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import ReactDOM from "react-dom";
+import "./scss/css/theme.min.css";
 import "./index.css";
 import App from "./App";
 
