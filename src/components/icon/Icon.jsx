c7b619377828dc298de45cddb2aaab097921fe90
landon mckell <landon.roney7923@gmail.com>
2022-01-04 22:29:32 -0700

    Task: Redesigned the contact page

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -2,50 +2,48 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Icon = (props) => {
-    function renderFontAwesomeStyle(style) {
-        switch (style) {
-            case "fas":
-                return "fas";
-            case "far":
-                return "far";
-            case "fab":
-                return "fab";
-            default:
-                return "fas";
-        }
+  function renderFontAwesomeStyle(style) {
+    switch (style) {
+      case "fas":
+        return "fas";
+      case "far":
+        return "far";
+      case "fab":
+        return "fab";
+      default:
+        return "fas";
     }
-    return (
-        <i
-            className={`${renderFontAwesomeStyle(props.iconStyle)} fa-${
-                props.icon ? `${props.icon}` : "exclamation-circle"
-            }`}
-            style={{
-                color: `${props.color ? `${props.color}` : "#ffffff"}`,
-                fontSize: `${props.size ? `${props.size}px` : "12px"}`,
-                marginTop: `${props.marginTop ? `${props.marginTop}px` : "0"}`,
-                marginBottom: `${
-                    props.marginBottom ? `${props.marginBottom}px` : "0"
-                }`,
-                marginRight: `${
-                    props.marginRight ? `${props.marginRight}px` : "0"
-                }`,
-                marginLeft: `${
-                    props.marginLeft ? `${props.marginLeft}px` : "0"
-                }`,
-            }}
-        />
-    );
+  }
+  return (
+    <i
+      className={`${renderFontAwesomeStyle(props.iconStyle)} fa-${
+        props.icon ? `${props.icon}` : "exclamation-circle"
+      }`}
+      style={{
+        color: `${props.color ? `${props.color}` : "#ffffff"}`,
+        fontSize: `${props.size ? `${props.size}px` : "12px"}`,
+        marginTop: `${props.marginTop ? `${props.marginTop}px` : "0"}`,
+        marginBottom: `${props.marginBottom ? `${props.marginBottom}px` : "0"}`,
+        marginRight: `${props.marginRight ? `${props.marginRight}px` : "0"}`,
+        marginLeft: `${props.marginLeft ? `${props.marginLeft}px` : "0"}`,
+        margin: `${props.margin ? `${props.margin}px` : 0}`,
+        cursor: `${props.isClickable ? "pointer" : "arrow"}`,
+      }}
+    />
+  );
 };
 
 Icon.propTypes = {
-    iconStyle: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-    color: PropTypes.string,
-    size: PropTypes.string || PropTypes.number,
-    marginTop: PropTypes.string || PropTypes.number,
-    marginBottom: PropTypes.string || PropTypes.number,
-    marginLeft: PropTypes.string || PropTypes.number,
-    marginRight: PropTypes.string || PropTypes.number,
+  iconStyle: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  isClickable: PropTypes.bool,
+  size: PropTypes.string || PropTypes.number,
+  marginTop: PropTypes.string || PropTypes.number,
+  marginBottom: PropTypes.string || PropTypes.number,
+  marginLeft: PropTypes.string || PropTypes.number,
+  marginRight: PropTypes.string || PropTypes.number,
+  margin: PropTypes.string || PropTypes.number,
 };
 
 export default Icon;
--- src/pages/contactPage/ContactPage.jsx
+++ src/pages/contactPage/ContactPage.jsx
@@ -4,140 +4,126 @@ import Icon from "../../components/icon/Icon";
 import ContactForm from "../../components/contactForm/ContactForm";
 
 const ContactPage = () => {
-    useEffect(() => {
-        window.scrollTo(0, 0, "smooth");
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0, "smooth");
+  }, []);
 
-    const socialMediaLinks = [
-        {
-            name: "Instagram",
-            link: "google.com",
-            icon: "instagram",
-        },
-        {
-            name: "Github",
-            link: "google.com",
-            icon: "github",
-        },
-        {
-            name: "LinkedIn",
-            link: "google.com",
-            icon: "linkedin",
-        },
-    ];
-
-    function renderSocialLinkColumns() {
-        return socialMediaLinks.map((social, idx) => (
-            <div className="col-md-4" key={idx}>
-                <a href={social.link} rel="noreferrer noopener" target="_blank" className="text-secondary">
-                    <Icon
-                        icon={social.icon}
-                        iconStyle="fab"
-                        size="40"
-                        color="#2d2d2d"
-                        marginBottom="15"
-                    />
-                    <p>{social.name}</p>
-                </a>
-            </div>
-        ));
-    }
-    function renderContactInformationCards() {
-        return (
-            <div
-                id="contactInformation"
-                className="col-md-6 order-xs-2 order-md-1"
-            >
-                <div className="row">
-                    <div className="col-12">
-                        <div className="card">
-                            <div className="card-body text-center">
-                                <Icon
-                                    icon="marker"
-                                    iconStyle="fas"
-                                    size="50"
-                                    color="#ffd700"
-                                    marginBottom="15"
-                                />
-                                <h3 className="text-muted text-uppercase pb-2">
-                                    Social Media
-                                </h3>
-                                <div className="row">
-                                    {renderSocialLinkColumns()}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row pt-4">
-                    <div className="col-6">
-                        <div className="card">
-                            <div className="card-body text-center">
-                                <Icon
-                                    icon="envelope"
-                                    iconStyle="fas"
-                                    size="50"
-                                    color="#ffd700"
-                                    marginBottom="15"
-                                />
-                                <h3 className="text-muted text-uppercase">
-                                    Email Us
-                                </h3>
-                                <p>afitech@test.com</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-6">
-                        <div className="card">
-                            <div className="card-body text-center">
-                                <Icon
-                                    icon="phone"
-                                    iconStyle="fas"
-                                    size="50"
-                                    color="#ffd700"
-                                    marginBottom="15"
-                                />
-                                <h3 className="text-muted text-uppercase">
-                                    Call Us
-                                </h3>
-                                <p>801-310-5876</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    return (
-        <div className="dkContactPage">
-            <section id="pageHeaderSection" className="bg-dark py-4">
-                <div className="container text-white">
-                    <div className="row">
-                        <div className="col-md-12 text-left">
-                            <h1 className="text-uppercase">Contact Us</h1>
-                            <p className="text-muted">
-                                If you have any questions or inquries, please
-                                don't hesitate to contact us! We'd love to hear
-                                you ideas and share them with the world!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section id="pageContactDetailsSection" className="bg-dark">
-                <div className="container py-3">
-                    <div className="row">
-                        {renderContactInformationCards()}
-                        <div className="col-md-6 order-xs-1 order-md-2 mb-2 text-white">
-                            <h4 className="mb-4">Leave us a message!</h4>
-                            <ContactForm />
-                        </div>
-                    </div>
-                </div>
-            </section>
+  return (
+    <div className="dkContactPage">
+      <section id="heroSection" className="py-5 bg-secondary text-white">
+        <div className="container">
+          <div className="row">
+            <h1 className="display-2 text-uppercase">Contact Us</h1>
+            <p className="text-white">
+              Let's connect and talk about your website and/or mobile app idea!
+            </p>
+          </div>
         </div>
-    );
+      </section>
+      <section id="contactSection" className="py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <h1 className="text-uppercase pb-4">Let's get in touch</h1>
+              <div id="addressInfo" className="mb-3">
+                <h6 className="text-muted text-uppercase">Address</h6>
+                <p>Spanish Fork, Utah, 84660</p>
+              </div>
+              <div id="emailInfo" className="mb-3">
+                <h6 className="text-muted text-uppercase">Email</h6>
+                <p>afitechfreelance.io@gmail.com</p>
+              </div>
+              <div id="phoneInfo" className="mb-3">
+                <h6 className="text-muted text-uppercase">Phone</h6>
+                <p>801-310-5876</p>
+              </div>
+              <div id="socialLinks" className="mb-3">
+                <Icon
+                  color="#333333"
+                  iconStyle="fab"
+                  icon="facebook"
+                  size={20}
+                  isClickable
+                  marginRight={20}
+                />
+                <Icon
+                  color="#333333"
+                  iconStyle="fab"
+                  icon="instagram"
+                  size={20}
+                  isClickable
+                  marginRight={20}
+                />
+                <Icon
+                  color="#333333"
+                  iconStyle="fab"
+                  icon="github"
+                  size={20}
+                  isClickable
+                  marginRight={20}
+                />
+                <Icon
+                  color="#333333"
+                  iconStyle="fab"
+                  icon="linkedin"
+                  size={20}
+                  isClickable
+                />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <ContactForm />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="featureSection" className="bg-dark text-white py-5">
+        <div className="container py-3">
+          <div className="row text-center">
+            <div className="col-4">
+              <div className="circle mx-auto mb-3">
+                <Icon
+                  iconStyle="fas"
+                  icon="microphone-alt"
+                  size={30}
+                  color="purple"
+                  margin="20"
+                />
+              </div>
+              <h3>Fast Response!</h3>
+              <p>We have an awesome reponse time to requests!</p>
+            </div>
+            <div className="col-4">
+              <div className="circle mx-auto mb-3">
+                <Icon
+                  iconStyle="fas"
+                  icon="handshake"
+                  size={30}
+                  color="purple"
+                  margin="20"
+                />
+              </div>
+              <h3>Let's Connect!</h3>
+              <p>Check out our social media!</p>
+            </div>
+            <div className="col-4">
+              <div className="circle mx-auto mb-3">
+                <Icon
+                  iconStyle="fas"
+                  icon="flag-usa"
+                  size={30}
+                  color="purple"
+                  margin="20"
+                />
+              </div>
+              <h3>U.S. Based Business</h3>
+              <p>We are proudly based in the United States</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ContactPage;
